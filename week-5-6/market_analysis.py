3a017b4dad6fe8d058d4d5593468f67372c039ee
marcdev <marcellkurniawan5@gmail.com>
2026-05-21 11:59:42 +0700

    MA crossover

--- week-5-6/market_analysis.py
+++ week-5-6/market_analysis.py
@@ -2,7 +2,18 @@ import pandas as pd
 import yfinance as yf
 from pandas_basics import analisis_saham, deteksi_anomali_volume, cek_harga_psikologis
 
+def cek_tren(df, fast=20, slow=50):
+    df = df.copy()
+    SMA_20 = df["Close"].rolling(window=fast).mean()
+    SMA_50 = df["Close"].rolling(window=slow).mean()
 
+    if SMA_20.iloc[-1] < SMA_50.iloc[-1]:
+        return "Downtrend"
+    elif SMA_20.iloc[-1] > SMA_50.iloc[-1]:
+        return "Uptrend"
+    else:
+        return "Neutral"
+    
 
 def main():
     stock = input("Stock: ").upper() + ".JK"
@@ -25,7 +36,7 @@ def main():
             f"jarak: {key_level['distance_percent']:.1%}, "
             f"dekat: {key_level['within_tolerance']}"
         )
-
+    print(f"Trend: {cek_tren(df)}")
 
 
 if __name__ == "__main__":
